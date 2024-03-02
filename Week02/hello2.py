74536fcc4d0a92f64127d1fd8822d90ebde5a058
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-02 22:58:40 +0000

    Programs reads name, say hello and nice to meet you

--- Week02/hello2.py
+++ Week02/hello2.py
@@ -1,10 +1,11 @@
 # hello2.py
-# This program reads in a person's name and prints out hello that persons name. Messing with inputs.
+# This program reads in a person's name and prints out Hello that persons name. 
 # Author: Noemi Diaz
 
 name = input ("say your name :")
 print ("Hello" + name)
 
+# This program reads in a person's name and prints out Hello and Nice to meet you
 
 name = input ("Enter your name:")
 print ('Hello'+ name + '\nNice to meet you')

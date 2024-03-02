ecd095e4f1458c181281764a971e615ddf9d3f36
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-02 22:43:27 +0000

    Program reads person's name and print out hello

---
+++ Week02/hello2.py
@@ -0,0 +1,8 @@
+# hello2.py
+# This program reads in a person's name and prints out hello that persons name. Messing with inputs.
+# Author: Noemi Diaz
+
+name = input ("say your name :")
+print ("Hello" + name)
+
+

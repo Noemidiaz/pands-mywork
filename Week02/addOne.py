6ddd597611dc59de005efc9b1b474cc9f03a8763
Noemi Diaz <noemi.diazglez@gmail.com>
2024-03-03 00:17:14 +0000

    Program reading on a number and prints out one more

---
+++ Week02/addOne.py
@@ -0,0 +1,11 @@
+# addOne.py
+# This program is reading on a number and prints out one more than that number
+# Author: Noemi Diaz
+
+number = int(input('please enter a number:'))
+newNumber = number + 1
+
+...
+print (f'{number} plus one is {newNumber}')
+...
+
